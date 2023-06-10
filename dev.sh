33b7a66c6518cde96c0d3d51e69812925738dd77
Michael van de Water <me@mjvdw.com>
2023-06-11 11:12:27 +1200

    Script for generating openapi client

--- dev.sh
+++ dev.sh
@@ -6,3 +6,7 @@ else
     echo "The environment does not exist. Creating environment."
     sudo apt install python3.10-venv -y
 fi
+
+source .venv/bin/activate
+pip install -r requirements.txt
+openapi-generator generate -i SpaceTraders.json -g python --skip-validate-spec -o backend/packages/spacetraders-sdk
