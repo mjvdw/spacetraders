2510afee963f05b4af468ea118bca5e27d7ef905
Michael van de Water <me@mjvdw.com>
2023-06-11 14:20:07 +1200

    Tidy up after implementing openapi client

--- backend/run.py
+++ backend/run.py
@@ -1,38 +1,8 @@
-# from manager import Manager
-# from db import Database
-# from worker import Worker, WorkerException
-# from instruction import Instruction
-
-
-# if __name__ == "__main__":
-#     # db = Database()
-#     # manager = Manager(db=db)
-
-#     # TEST = {
-#     #     "name": "My agent details",
-#     #     "api_command": "get_my_agent_details",
-#     #     "params": None,
-#     # }
-
-#     # manager.next_instruction = Instruction(TEST)
-
-#     # if manager.has_instruction:
-#     #     worker = Worker(manager.next_instruction)
-#     #     try:
-#     #         worker.go()
-#     #     except WorkerException as e:
-#     #         print(e)
-
-#     pass
-
 from __future__ import print_function
-
-import time
+import os
 import openapi_client
-
 from openapi_client.rest import ApiException
 from pprint import pprint
-import os
 from dotenv import load_dotenv
 
 
