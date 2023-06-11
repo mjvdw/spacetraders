1e810bcc92b77d81f2c111d07e8bd2995052dd2a
Michael van de Water <me@mjvdw.com>
2023-06-11 14:28:00 +1200

    Updates to dev.sh for better initial set up

--- dev.sh
+++ dev.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-if [ -d ".venv" ]; then
-    echo "Activating environment."
-else
-    echo "The environment does not exist. Creating environment."
+if [ ! -d ".venv" ]; then
     sudo apt install python3.10-venv -y
 fi
 
 source .venv/bin/activate
 pip install -r requirements.txt
 openapi-generator generate -i SpaceTraders.json -g python --skip-validate-spec -o packages/spacetraders-sdk
+pip install packages/spacetraders-sdk
